8bc88fe0b222e8190c6be6706e733b645737f609
Erick Santos <erickgaves@gmail.com>
2020-10-27 13:25:51 -0300

    resolucoes ate o exercicio 4

---
+++ bloco_04/introduao-a-javascript-e-logica-de-programacao_1/exercicio_1.js
@@ -0,0 +1,52 @@
+console.log("---------------------------")
+console.log('Exercício 01')
+console.log("---------------------------")
+
+let a =  10;
+let b = 20;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(b % a );
+console.log("---------------------------")
+console.log('Exercício 02')
+console.log("---------------------------")
+
+let x = 100;
+let y = 1000;
+
+if (x > y) {
+    console.log(x);
+} else {
+    console.log(y);
+}
+console.log("---------------------------")
+console.log('Exercício 03')
+console.log("---------------------------")
+
+a = 4;
+b = 5;
+let c = 3;
+
+if (a > b && a > c ) {
+    console.log(a);
+}   else if (b > a && b > c) {
+    console.log(b);
+}   else {
+    console.log(c);
+}
+console.log("---------------------------")
+console.log('Exercício 04')
+console.log("---------------------------")
+
+x = 23;
+
+if (x > 0) {
+    console.log("positive");
+}   else if (x < 0) {
+    console.log("negative");
+}   else {
+    console.log("zero");
+}
