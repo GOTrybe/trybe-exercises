49aeb60c9be6a29166bd329e54ab0627ca1c0e06
Erick Santos <erickgaves@gmail.com>
2020-10-27 18:55:43 -0300

    inserindo respostas

--- bloco_04/inntroducao-a-javascript-e-logica-de-programaca0_3/exercicio_03.js
+++ bloco_04/inntroducao-a-javascript-e-logica-de-programaca0_3/exercicio_03.js
@@ -15,5 +15,68 @@ if (n > 1) {
 }
 
 console.log("-------------------------------")
-console.log("Exercício 02 - Formar quadrado.")
+console.log("Exercício 02 - Formar triângulo retângulo.")
 console.log("-------------------------------")
+
+n = 5;
+
+console.log(`n = ${n}`)
+console.log("")
+
+if (n > 1) {
+    console.log("*")
+    console.log("**")
+    console.log("***")
+    console.log("****")
+}
+
+console.log("-------------------------------")
+console.log("Exercício 03 - Formar triângulo retângulo invertido.")
+console.log("-------------------------------")
+
+n = 5;
+
+console.log(`n = ${n}`)
+console.log("")
+
+if (n > 1) {
+    console.log(" ", " ", " ", " ","*")
+    console.log(" ", " ", " ", "*", "*")
+    console.log(" "," ", "*", "*", "*")
+    console.log(" ","*", "*", "*", "*")
+    console.log("*", "*", "*", "*", "*")
+}
+
+console.log("-------------------------------")
+console.log("Exercício 04 - Formar pirâmide.")
+console.log("-------------------------------")
+
+n = 5;
+
+console.log(`n = ${n}`)
+console.log("")
+
+if (n > 1) {
+    console.log("", "", "", "","*")
+    console.log("", "", "", "*", "*")
+    console.log("","", "*", "*", "*")
+    console.log("","*", "*", "*", "*")
+    console.log("*", "*", "*", "*", "*")
+}
+
+console.log("-------------------------------")
+console.log("Exercício 05 - Formar pirâmide vazia no centro.")
+console.log("-------------------------------")
+
+n = 5;
+
+console.log(`n = ${n}`)
+console.log("")
+
+if (n > 1) {
+    console.log("", "", "", "","*")
+    console.log("", "", "", "*", "*")
+    console.log("","", "*", "","", "*")
+    console.log("", "*","", "", "", "", "*")
+    console.log("*", "*", "*", "*", "*")
+}
