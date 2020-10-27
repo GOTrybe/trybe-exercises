3da3f98afa53728e8765655cbda9d9b204c86cbe
Erick Santos <erickgaves@gmail.com>
2020-10-27 18:37:17 -0300

    insert exercicio 03

---
+++ bloco_04/inntroducao-a-javascript-e-logica-de-programaca0_3/exercicio_03.js
@@ -0,0 +1,19 @@
+console.log("-------------------------------")
+console.log("Exercício 01 - Formar quadrado.")
+console.log("-------------------------------")
+
+let n = 2;
+
+console.log(`n = ${n}`)
+console.log("")
+
+if (n > 1) {
+    console.log("****")
+    console.log("****")
+    console.log("****")
+    console.log("****")
+}
+
+console.log("-------------------------------")
+console.log("Exercício 02 - Formar quadrado.")
+console.log("-------------------------------")
